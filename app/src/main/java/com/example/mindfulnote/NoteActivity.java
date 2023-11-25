b659967827801e81739ef6ceeb6a2b747884560f
codingbecky <beckyjin873@gmail.com>
2023-11-25 00:53:58 -0800

    - Note list in random color - Edit note and note detail activity(not implemented yet) - Note menu

--- app/src/main/java/com/example/mindfulnote/NoteActivity.java
+++ app/src/main/java/com/example/mindfulnote/NoteActivity.java
@@ -1,17 +1,22 @@
 package com.example.mindfulnote;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
@@ -20,6 +25,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+
+import java.util.Random;
 
 public class NoteActivity extends AppCompatActivity {
     FloatingActionButton mcreatenotesfab;
@@ -50,8 +57,42 @@ public class NoteActivity extends AppCompatActivity {
         noteAdapter = new FirestoreRecyclerAdapter<firebasemodel, NoteViewHolder>(allusernotes) {
             @Override
             protected void onBindViewHolder(@NonNull NoteViewHolder holder, int position, @NonNull firebasemodel model) {
+                ImageView popupbutton = holder.itemView.findViewById(R.id.menupopbutton);
+                int colorcode = getRandomColor();
+                holder.mnote.setBackgroundColor(holder.itemView.getResources().getColor(colorcode, null));
                 holder.notetitle.setText(model.getTitle());
                 holder.notecontent.setText(model.getContent());
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(v.getContext(), NoteDetails.class);
+                        v.getContext().startActivity(intent);
+
+                    }
+                });
+                popupbutton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        PopupMenu popupMenu = new PopupMenu(v.getContext(), v);
+                        popupMenu.setGravity(Gravity.END);
+                        popupMenu.getMenu().add("Edit").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                            @Override
+                            public boolean onMenuItemClick(@NonNull MenuItem item) {
+                                Intent intent = new Intent(v.getContext(), EditNoteActivity.class);
+                                v.getContext().startActivity(intent);
+                                return false;
+                            }
+                        });
+                        popupMenu.getMenu().add("Delete").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                            @Override
+                            public boolean onMenuItemClick(@NonNull MenuItem item) {
+                                Toast.makeText(v.getContext(), "This note is deleted", Toast.LENGTH_SHORT).show();
+                                return false;
+                            }
+                        });
+                        popupMenu.show();
+                    }
+                });
             }
 
             @NonNull
@@ -91,8 +132,17 @@ public class NoteActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        if(noteAdapter != null){
+        if (noteAdapter != null) {
             noteAdapter.stopListening();
         }
     }
+
+    private int[] colorcodes = new int[]{R.color.color1, R.color.color2, R.color.color3, R.color.color4, R.color.color5, R.color.green, R.color.grey, R.color.lightgreen, R.color.skyblue, R.color.pink};
+
+    private int getRandomColor() {
+        Random rand = new Random();
+        int number = rand.nextInt(colorcodes.length);
+        return colorcodes[number];
+    }
+
 }
